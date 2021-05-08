21379d8e1bd0c55935cedd0dabd8f43d39a7d0a1
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-05-09 00:46:18 +0500

    created simple server using express with simple get request

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 //creating mikro-orm instance it return promise
 
 const main = async () => {
+  //database connection
   const mikroORM = await MikroORM.init(mikroConfig);
-
   //run migrations
   await mikroORM.getMigrator().up();
+
+  // create express server
+
+  const app = express();
+
+  //simple get request
+  app.get("/", (_req, res) => {
+    return res.status(200).json({ message: "Assalam o Aliekum" });
+  });
+
+  app.listen(3000, () => console.log("listening on PORT 3000"));
 
   //   create simple post
 
@@ -17,8 +29,8 @@ const main = async () => {
   //   await mikroORM.em.persistAndFlush(post);
 
   //get all posts
-  const posts = await mikroORM.em.find(Post, {});
-  console.log("posts", posts);
+  // const posts = await mikroORM.em.find(Post, {});
+  // console.log("posts", posts);
 };
 
 main().catch((err) => console.error("error", err));
