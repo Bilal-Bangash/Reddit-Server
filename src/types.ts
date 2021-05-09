9ff07ac8133674bc805b3131256127470a9d86e2
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-05-09 20:18:36 +0500

    session authentication using express-session and redis, connect-redis

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
+import { Session } from "express-session";
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session?: Session & { userId?: number } };
+  res: Response;
 };
